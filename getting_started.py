e12f706252b58fc16908ce599f34e6a044ff56b5
Timmecom <oluyoyin2000@gmail.com>
2021-08-09 23:10:22 +0100

    Updated getting_started.py

---
+++ getting_started.py
@@ -0,0 +1,16 @@
+'''
+This is my first file in this repo and thus first experience with git environment
+The name 'automatic-barnacle' was even a suggestion
+
+Libraries to be used will be imported below:
+'''
+import math
+import random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy as sc
+import beautifulsoup4 as bs4
+
+#etc
